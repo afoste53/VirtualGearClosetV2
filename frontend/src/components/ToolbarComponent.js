96dee3d881b3a9e0093e45ecefbbc99bfb3183ac
Gabe <afoste53@gmail.com>
2021-10-03 05:10:16 -0500

    add add gear and closet buttons

--- frontend/src/components/ToolbarComponent.js
+++ frontend/src/components/ToolbarComponent.js
@@ -1,10 +1,31 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import GearSearchComponent from "./GearSearchComponent";
 
 const ToolbarComponent = () => {
+  const createNewCloset = (e) => {};
+
+  const addGear = (e) => {};
+
   return (
     <Col className="toolbar bg-light-blue">
       <div>
+        <div className="d-flex align-items-center justify-content-center">
+          <button className="btn btn-sm toolbar-add-button" onClick={addGear}>
+            <span>
+              <i className="fas fa-plus" />
+            </span>{" "}
+            Add Gear
+          </button>
+          <button
+            className="btn btn-sm toolbar-add-button bg-light-blue"
+            onClick={createNewCloset}
+          >
+            <span>
+              <i className="fas fa-plus" />
+            </span>{" "}
+            New Closet
+          </button>
+        </div>
         <Row>
           <GearSearchComponent />
         </Row>
