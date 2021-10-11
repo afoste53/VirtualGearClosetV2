a321b79463dd684b159186dc1fed25d9a87332c8
Gabe <afoste53@gmail.com>
2021-10-11 00:03:53 -0500

    add delete gear controller

--- server/Controllers/gearController.js
+++ server/Controllers/gearController.js
@@ -42,7 +42,41 @@ const createGear = asyncHandler(async (req, res) => {
 
 // @desc        Delete a piece of gear
 // @route       DELETE /api/gear/:id/delete/:gearId
-const deleteGear = asyncHandler(async (req, res) => {});
+const deleteGear = asyncHandler(async (req, res) => {
+  const user = await verifyUserExists(req.params.id, res);
+
+  const gearId = req.params.gearId;
+  const g = user.gear.find((c) => c.gear_id == gearId);
+
+  if (!g) {
+    res.status(404).json({
+      Success: false,
+      Error: `No gear found with id ${gearId}`,
+    });
+    return;
+  }
+
+  // walk through closet array of the gear we are trying to delete
+  // for each closetId in there, go find them on the user object and
+  // filter the gearId of what we need to remove out of the gearInCloset
+  // array
+  g.closets.forEach((c) => {
+    let curr = user.closets.filter((i) => c.toString() == i._id.toString())[0];
+    curr.gearInCloset = curr.gearInCloset.filter(
+      (j) => j.toString() !== gearId.toString()
+    );
+  });
+
+  // remove the gear item from the users array of gear
+  user.gear = user.gear.filter((g) => g.gear_id != gearId);
+
+  await user.save();
+  res.status(202).json({
+    Success: true,
+    Message: `${g.gearName} has been deleted`,
+    user,
+  });
+});
 
 // @desc        Update a piece of gear
 // @route       PUT /api/gear/:id/update/:gearId
